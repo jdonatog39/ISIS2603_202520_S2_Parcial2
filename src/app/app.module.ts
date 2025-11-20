283711e8427afbccd69b06946da9f95bb0e170a9
jdonatog39 <j.donatog@uniandes.edu.co>
2025-11-20 07:53:28 -0500

    subido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieModule } from './movie/movie.module';
+import { movieRoutingModule } from './movie/movie-routing-module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MovieModule],
+  imports: [BrowserModule, AppRoutingModule, MovieModule,movieRoutingModule],
   providers: [MovieModule],
   bootstrap: [AppComponent],
 })
